40a04ecedaf7234d80f57dcb55273cc5b9d7e92f
Tejas-Bauskar <tejasbauskar2@gmail.com>
2022-03-30 10:59:31 +0530

    Done with test

---
+++ Demo/src/test/Test1.java
@@ -0,0 +1,12 @@
+package test;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+	int a = 5;
+	int b=10;
+	System.out.println(a*b);
+
+	}
+
+}
---
+++ Demo/src/test/Test3.java
@@ -0,0 +1,12 @@
+package test;
+
+public class Test3 {
+
+	public static void main(String[] args) {
+		double a=2.5;
+		double b=3.5;
+		System.out.println(a*b);
+		
+	}
+
+}
